51baaa0db568d4bebfc60ab0be0925cb46d59bcc
fmiguelgarcia <francisco.miguel.garcia.rodriguez@gmail.com>
2017-05-19 17:51:17 +0200

    Adding Windows support

--- qxmpp/conanfile.py
+++ qxmpp/conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, tools
 import shutil
-import multiprocessing
 
 class QXmppConan(ConanFile):
     # Conan 
@@ -33,7 +32,8 @@ class QXmppConan(ConanFile):
         shutil.move( "qxmpp-0.9.3", self.source_path)
 
     def build(self):
-        qmake_options = "PREFIX=" + self.prefix + " "
+        qmake_options = "CONFIG+=debug " if self.settings.build_type == "Debug" else "CONFIG+=release "
+        qmake_options += "CONFIG+=warn_on " 
         qmake_options += "QXMPP_AUTOTEST_INTERNAL=1 " if self.options.autotest_internal else ""
         qmake_options += "QXMPP_USE_DOXYGEN=1 " if self.options.use_doxygen else ""
         qmake_options += "QXMPP_USE_OPUS=1 " if self.options.use_opus else ""
@@ -43,12 +43,26 @@ class QXmppConan(ConanFile):
         
         with tools.chdir( "qxmpp"):
             self.run( "qmake \"%s\"" % qmake_options)
-            self.run( "make -j %d" % multiprocessing.cpu_count())
-            self.run( "make install")
+
+            if self.settings.compiler == "Visual Studio":
+                with tools.environment_append( {"CL": "/MP"}):
+                    self.run( "nmake")
+            else:
+                self.run( "make -j %d" % tools.cpu_count())
 
     def package(self):
-        self.copy( pattern="*.h", src="qxmpp/src/usr", dst="include/", keep_path=True)
-        self.copy( pattern="libqxmpp.so*", src="qxmpp/src", dst="lib/", keep_path=True, symlinks=True)
+        self.copy( pattern="*.h", src="qxmpp/src/base", dst="include/", keep_path=True)
+        self.copy( pattern="*.h", src="qxmpp/src/client", dst="include/", keep_path=True)
+        self.copy( pattern="*.h", src="qxmpp/src/server", dst="include/", keep_path=True)
+        if self.settings.compiler == "Visual Studio":
+            for lib_ext in [ "dll", "pdb", "exp", "lib"]:
+                self.copy( pattern="qxmpp*.%s" % lib_ext, src="qxmpp/src", dst="lib/", keep_path=False, symlinks=True)
+        else:
+            self.copy( pattern="*qxmpp.so*", src="qxmpp/src", dst="lib/", keep_path=False, symlinks=True)
 
     def package_info(self):
-        self.cpp_info.libs.extend(["qxmpp"])
+        lib_name = "qxmpp"
+        if self.settings.compiler == "Visual Studio":
+            lib_name = "qxmpp0" if self.settings.build_type == "Release" else "qxmpp_d0"
+        self.cpp_info.libs.extend([lib_name])
+
---
+++ qxmpp/test_package/main.cpp
@@ -0,0 +1,8 @@
+#include <QXmppClient.h>
+
+int main(int argc, char *argv[])
+{
+   QXmppClient client = new QXmppClient();
+
+   return 0;
+}
